b0df949e26d76c078af890a89861029d04b71087
sidechain-studio <studio@sidechain.studio>
2017-01-30 15:29:21 +1100

    new commit

--- public/js/customScripts.js
+++ public/js/customScripts.js
@@ -5,9 +5,21 @@ $( document ).ready(function() {
 	menuBtn.addEventListener('click', function() {
 	    $('.outer-container').toggleClass('blur');
 	}, false),
+	menuBtn.addEventListener('click', function() {
+	    $('.sc-nav-menu').toggleClass('bg-init');
+	}, false),
+	menuBtn.addEventListener('click', function() {
+	    $('.sc-nav-collapse').toggleClass('vsbl');
+	}, false),
 
 	menuListItem.addEventListener('click', function() {
 		$('.outer-container').removeClass('blur');
+	}, false);
+	menuListItem.addEventListener('click', function() {
+		$('.sc-nav-menu').removeClass('bg-init');
+	}, false);
+	menuListItem.addEventListener('click', function() {
+		$('.sc-nav-collapse').removeClass('vsbl');
 	}, false);
 
 });
