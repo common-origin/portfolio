57fb45e86fa669e2aa4c51b980d4b10872286b32
sidechain-studio <studio@sidechain.studio>
2017-05-08 20:54:18 +1000

    V79

--- public/js/customScripts.js
+++ public/js/customScripts.js
@@ -72,7 +72,6 @@ $( document ).ready(function() {
   //caches a jQuery object containing the header element
   var header = $(".navbar-default");
   var pageHead = $(".page-nav-cont");
-  //var pageTitle = $(".title-row");
   var sidebar = $(".sidebar-menu");
   var pagination = $(".pagination");
 
@@ -108,3 +107,23 @@ $( document ).ready(function() {
   });
 }); 
 
+
+
+// Back top top button 
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
