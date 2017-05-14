de13b4f2f88004b02e36384efcf72ff4489bf55d
sidechain-studio <studio@sidechain.studio>
2017-05-14 19:51:58 +1000

    V100

--- public/js/homeScripts.js
+++ public/js/homeScripts.js
@@ -1,21 +1,3 @@
-// Entry Button Animation Trigger
-
-//$( document ).ready(function() {
-//   var enterBtn = document.getElementById('enter-btn');
-//
-//	enterBtn.addEventListener('click', function() {
-//	    $('.menu-bg').toggleClass('expand'),
-//	    $('.enter-site-btn').toggleClass('switch'),
-//	    $('.work-btn').toggleClass('switch'),
-//	    $('canvas').toggleClass('switch'),
-//	    $('.title-cont').addClass('allow');
-//	}, false);
-//	
-//	enterBtn.addEventListener('click', function() {
-//	    $('.sc-logo-cont').toggleClass('svg-anim');
-//	}, false);
-//});
-
 // SEND PAGE BACK TO TOP ON REFRESH
 $(window).load(function() {
 	window.onbeforeunload = function() {window.scrollTo(0,0);}
