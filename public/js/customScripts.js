ee2dab39a16bcc22673ab745bcd83032185b5683
sidechain-studio <studio@sidechain.studio>
2017-05-16 16:42:53 +1000

    V120

--- public/js/customScripts.js
+++ public/js/customScripts.js
@@ -2,7 +2,7 @@
 
 $( document ).ready(function() {
   var newClass = window.location.href;
-  newClass = newClass.substring(newClass.lastIndexOf('http://localhost:3000')+24, 28);
+  newClass = newClass.substring(newClass.lastIndexOf('http://localhost:3000')+25, 29);
   $('body').addClass(newClass);
 });
 
