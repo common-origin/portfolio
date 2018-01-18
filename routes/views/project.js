9b8bbdf64f0f6b00ed33f255716fe67c5bb13b38
Alexander J <johansen0711@hotmail.com>
2018-01-18 12:07:06 +0100

    - completed the ticket "Fix the next/previous project buttons on project pages "

--- routes/views/project.js
+++ routes/views/project.js
@@ -13,6 +13,11 @@ exports = module.exports = function (req, res) {
 		projects: [],
 	};
 
+	locals.selected = {
+		previous: {},
+		next: {},
+	};
+
 	// Load the current project
 	view.on('init', function (next) {
 
@@ -20,6 +25,8 @@ exports = module.exports = function (req, res) {
 			state: 'published',
 			slug: locals.filters.project,
 		}).populate('author categories');
+
+		locals.data.projects = q;
 
 		q.exec(function (err, result) {
 			locals.data.project = result;
@@ -31,13 +38,32 @@ exports = module.exports = function (req, res) {
 	// Load other posts
 	view.on('init', function (next) {
 
-		var q = keystone.list('Portfolio').model.find().where('state', 'published').sort('-publishedDate').populate('author').limit('2');
-
+		var q = keystone.list('Portfolio').model.find().where('state', 'published').sort('-publishedDate').populate('author').limit('100');
+		
 		q.exec(function (err, results) {
 			locals.data.projects = results;
+
+			for (var i = 0; i < locals.data.projects.length; i ++){
+				if(locals.data.projects[i]._id.toString() == locals.data.project._id.toString()){
+					
+					if(i == 0){
+						locals.selected.previous = locals.data.projects[0];
+					}else {
+						locals.selected.previous = locals.data.projects[i-1];
+					}
+
+					if(i == locals.data.projects.length - 1){
+						locals.selected.next = locals.data.projects[locals.data.projects.length - 1];
+					}else {
+						locals.selected.next = locals.data.projects[i + 1];
+					}
+					
+				} else {
+				}
+			}
+
 			next(err);
 		});
-
 	});
 
 	// Render the view
