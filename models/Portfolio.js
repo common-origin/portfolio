68675cd0a61d6e84d6dbe1e10e0e355d3a027c8f
Sidechain <ollie@sidechain.studio>
2018-05-09 12:35:19 +1000

    link-btn

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -19,8 +19,8 @@ Portfolio.add({
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	name: { type: Types.Text },
 	Url:{ type: Types.Url },
-	images: { type: Types.CloudinaryImages, publicID: 'slug' },
-	image: { type: Types.CloudinaryImage },
+	images: { type: Types.CloudinaryImages, publicID: 'slug', secure: 'true' },
+	image: { type: Types.CloudinaryImage, secure: 'true' },
 	videoUrl: { type: Types.Url },
 	videoData: { type: Types.Embedly, from: 'videoUrl' },
 	content: {
